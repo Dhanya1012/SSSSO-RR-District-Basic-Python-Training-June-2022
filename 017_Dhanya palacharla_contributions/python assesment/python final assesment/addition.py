0ea6e77fca4a2c01a1de7e29dc1cda08d9f40069
Dhanya1012 <107975719+Dhanya1012@users.noreply.github.com>
2022-07-24 16:10:53 +0530

    Add files via upload

---
+++ 017_Dhanya palacharla_contributions/python assesment/python final assesment/addition.py
@@ -0,0 +1,12 @@
+def add(num1,num2,num3,num4):
+    
+    addsum=0
+    if(num1%2==0):
+        addsum=addsum+num1
+    if(num2%2==0):
+        addsum=addsum+num2
+    if(num3%2==0):
+        addsum=addsum+num3
+    if(num4%2==0):
+        addsum=addsum+num4
+    return(addsum)
---
+++ 017_Dhanya palacharla_contributions/python assesment/python final assesment/multiplication.py
@@ -0,0 +1,12 @@
+def multiply(num1,num2,num3,num4):
+    
+    mul=1
+    if(num1%2==0):
+        mul=mul*num1
+    if(num2%2==0):
+        mul=mul*num2
+    if(num3%2==0):
+        mul=mul*num3
+    if(num4%2==0):
+        mul=mul*num4
+    return(mul)
